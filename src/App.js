f9d00bee36c225bf34f715e06be41f64b08e2359
aljagne <ajagne01@gmail.com>
2022-06-29 20:05:02 +0100

    Created Header and styled it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import Header from "./components/Header";
+// import Menu from "./components/Menu";
+// import Content from "./components/Content";
+
+const StyledMainWrapper = styled.div`
+  position: relative;
+`;
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledMainWrapper>
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      {/* <Content />
+      <Menu isMenuOpen={isMenuOpen} /> */}
+    </StyledMainWrapper>
   );
 }
 
